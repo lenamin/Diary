8789dc1dd80fd3a08e3f9085c8396e22feb140cc
Lena <99120199+lenamin@users.noreply.github.com>
2022-07-05 14:33:41 +0900

    진행상황 반영

---
+++ Diary.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+
+// 일기장에 작성한 내용을 collection view로 나타내기 위한 구조체를 정의한다
+
+struct Diary {
+    var title: String // 일기의 제목을 저장한다
+    var contents: String // 내용을 저장한다
+    var date: Date // 일기가 작성된 날짜를 저장한다
+    var isStar: Bool // 즐겨찾기 여부를 결정한다
+}
